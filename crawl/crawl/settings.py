b596d014cde14e413b629bfae8aaabd5feae5fbc
glindste <gustaflindstedt@gmail.com>
2013-01-13 16:28:38 +0100

    initial scrapy commit

---
+++ crawl/crawl/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for crawl project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/topics/settings.html
+#
+
+BOT_NAME = 'crawl'
+
+SPIDER_MODULES = ['crawl.spiders']
+NEWSPIDER_MODULE = 'crawl.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'crawl (+http://www.yourdomain.com)'
